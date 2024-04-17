0344e72c3c7525d353b3947b2f3f91f78ec8d798
Ben-Legg <benlegg2002@gmail.com>
2024-04-17 15:31:40 +0100

    Changed endpoint accesses for market information

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 const apiKey = "CG-EBE4jd5B73HeK6RLAiNR1pYN";
 
-
-async function requestCoinInfo (id)  {
+async function requestCoinInfo (id, currency)  {
     try {
-        const res = await axios.get(`https://api.coingecko.com/api/v3/coins/${id}`, {
-            headers: {'x-cg-demo-api-key': apiKey,}
+        const res = await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&ids=${id}`, {
+            headers: {'x-cg-demo-api-key': apiKey}
         });
-        return res.data;
+        for (const coin of res.data) {
+            console.log(coin.id);
+            console.log(`${coin.symbol}/${currency}`);
+            console.log(coin.current_price);
+            console.log(coin.image);
+            console.log(coin.price_change_percentage_24h);
+        }
     } catch (e) {
         console.log("ERROR", e);
     }
 };
-
-async function displayCoinInfo (id) {
-    const coinData = await requestCoinInfo(id);
-    console.log(coinData);
-}
-displayCoinInfo("bitcoin");
