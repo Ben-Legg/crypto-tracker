9f34a085042141c21e103aa88c254f47584a18c5
Ben-Legg <benlegg2002@gmail.com>
2024-04-19 16:13:50 +0100

    Improve readability and naming

--- app.js
+++ app.js
@@ -9,64 +9,51 @@ function convertTime(timestamp) { // Converts ISO 8601 format date to readable f
 }
 
 function buildTile(coin, currency){ // Function to create a tile using object data
-    const list = document.querySelector(".tracked-coins");
+    const trackedCoins = document.querySelector(".tracked-coins");
 
-    const tile = document.createElement("div"); // Create tile for coin
+    const tile = document.createElement("div"); // Create coin tile
     tile.classList.add("tile");
 
-    const imgContainer = document.createElement("div"); // Create image element and container
+    const imgContainer = document.createElement("div"); // Create image container
     imgContainer.classList.add("img-container");
-    const coinImg = document.createElement("img");
+    const coinImg = document.createElement("img"); // Create image element
     coinImg.classList.add("coin-logo");
     coinImg.setAttribute("src", coin.image);
     coinImg.setAttribute("alt", `${coin.id} icon`);
     imgContainer.appendChild(coinImg);
-    tile.appendChild(imgContainer); // Append to tile
+    tile.appendChild(imgContainer); // Append image container to tile
 
-    const nameContainer = document.createElement("div"); // Create name elements and container
+    const nameContainer = document.createElement("div"); // Create name container
     nameContainer.classList.add("name-container");
-    const symbol = document.createElement("h3");
-    symbol.textContent = `${coin.symbol.toUpperCase()}/${currency}`;
+    const symbol = document.createElement("h3"); // Create coin/currency pairing element
+    symbol.textContent = `${coin.symbol.toUpperCase()} / ${currency.toUpperCase()}`;
     nameContainer.appendChild(symbol);
-    const name = document.createElement("p");
+    const name = document.createElement("p"); // Create coin name element
     name.textContent = coin.id;
     nameContainer.appendChild(name);
-    const lastUpdatedDate = document.createElement("p");
+    const lastUpdatedDate = document.createElement("p"); // Create timestamp element
     lastUpdatedDate.textContent = convertTime(`${coin["last-updated"]}`);
     nameContainer.appendChild(lastUpdatedDate);
-    tile.appendChild(nameContainer); // Append to tile
+    tile.appendChild(nameContainer); // Append name container to tile
 
-    const priceContainer = document.createElement("div"); // Create Price element and container
+    const priceContainer = document.createElement("div"); // Create price container
     priceContainer.classList.add("price-container");
-    const price = document.createElement("h3");
+    const price = document.createElement("h3"); // Create price element
     price.textContent = coin.price;
     priceContainer.appendChild(price);
 
-    const priceChange = document.createElement("p");
+    const priceChange = document.createElement("p"); // Create price change element
     priceChange.classList.add("price-change");
     priceChange.textContent = `${coin['price-change'].toFixed(2)}`;
     priceContainer.appendChild(priceChange);
-    const priceChangePercentage = document.createElement("p");
+    const priceChangePercentage = document.createElement("p"); // Create percentage price change element
     priceChangePercentage.classList.add("price-change-percentage");
     priceChangePercentage.textContent = `${coin['price-change-percentage'].toFixed(2)}%`;
     priceContainer.appendChild(priceChangePercentage);
-    tile.appendChild(priceContainer); // Append to tile
+    tile.appendChild(priceContainer); // Append price container to tile
 
-    list.appendChild(tile);
+    trackedCoins.appendChild(tile); // Append tile to tracked coins
 }
-
-// [NEED BACK END, REVISIT LATER IN TIME TO ADD DATA PERSISTENCE]
-// async function loadCoins() { // function to load coins from JSON file
-//     try {
-//         const response = await fetch("coins.json"); // Fetch the JSON data from coins.json
-//         const data = await response.json(); // Parse the JSON data
-//         data.forEach(coin => { // Build tile for each coin object
-//           buildTile(coin);
-//         });
-//       } catch (error) {
-//         console.error("Error: Failed to load coins from coins.json");
-//       }
-// }
 
 async function requestCoinInfo (id, currency)  {
     try {
@@ -102,7 +89,7 @@ searchForm.addEventListener('submit', function (e) {
     const coinSearch = coinInput.value.toLowerCase();
 
     const currencyInput = searchForm.elements.currency;
-    const currencySearch = currencyInput.value.toUpperCase();
+    const currencySearch = currencyInput.value;
     
     requestCoinInfo(coinSearch, currencySearch);
     coinInput.value = "";
