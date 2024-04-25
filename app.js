e1d2075a282ac0631caa70b52bef39fa84b3c931
Ben-Legg <benlegg2002@gmail.com>
2024-04-25 18:06:36 +0100

    Updated comments

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function convertTime(timestamp) { // Converts ISO 8601 format date to readable f
     return formattedDate;
 }
 
-function dynamicRound(price) {
+function dynamicRound(price) { // Function to dynamically round price changes to decimal points based on magnitude of price change
     if (Math.abs(price) < 0.01) {
         return price.toFixed(8); // Round to 8 decimal places for very low prices
     } else if (Math.abs(price) < 1) {
@@ -20,46 +20,44 @@ function dynamicRound(price) {
     }
 }
 
-function addPriceChangeClass(element) {
+function addPriceChangeClass(element) { // Function to add pos/neg style to element based on price change value
     const value = parseFloat(element.textContent);
-    if (value > 0) {
+    if (value > 0) { // Apply pos price class to price changes above 0
         element.classList.add("pos-price-change");
         element.classList.remove("neg-price-change");
-    } else if (value < 0) {
+    } else if (value < 0) { // Apply neg price class to price changes below 0
         element.classList.add("neg-price-change");
         element.classList.remove("pos-price-change");
-    } else {
+    } else { // Remove all price classes to price changes === 0
         element.classList.remove("pos-price-change");
         element.classList.remove("neg-price-change");
     }
 }
 
-function tileHoverEffects(tile) {
-    tile.addEventListener("mouseenter", () => {
-        tile.classList.add("tile-hover");
-        const img = tile.querySelector('.img-container img');
-        img.classList.add('tile-hover-img');
-        setTimeout(() => {
-            img.classList.remove('tile-hover-img');
-        }, 900);
-    });
-    
-    tile.addEventListener('mouseleave', () => {
-        tile.classList.remove("tile-hover");
-    });
-}
-
-
-function colourPriceChanges() {
+function colourPriceChanges() { // Function to apply price change styles to all price changes and price change percentages
     const priceChanges = document.querySelectorAll(".price-change");
     for (const priceChange of priceChanges) {
         addPriceChangeClass(priceChange);
     }
-
     const priceChangePercentages = document.querySelectorAll(".price-change-percentage");
     for (const priceChangePercentage of priceChangePercentages) {
         addPriceChangeClass(priceChangePercentage);
     }
+}
+
+function tileHoverEffects(tile) { // Function to apply hover effects to coin tiles
+    tile.addEventListener("mouseenter", () => { // Apply tile syles and img animations upon hover
+        tile.classList.add("tile-hover");
+        const img = tile.querySelector('.img-container img');
+        img.classList.add('tile-hover-img');
+        setTimeout(() => { // Remove img animation class after animation has finished
+            img.classList.remove('tile-hover-img');
+        }, 900);
+    });
+    
+    tile.addEventListener('mouseleave', () => { // Remove tile hover styles when mouse leaves tile
+        tile.classList.remove("tile-hover");
+    });
 }
 
 function buildTile(coin, currency){ // Function to create a tile using object data
@@ -109,35 +107,34 @@ function buildTile(coin, currency){ // Function to create a tile using object da
 
     trackedCoins.appendChild(tile); // Append tile to tracked coins
 
-    tileHoverEffects(tile);
+    tileHoverEffects(tile); // Apply hover effects to tile
 
-    colourPriceChanges();
+    colourPriceChanges(); // Apply price change styles to tile price changes
 }
 
-function searchWarning(warningMessage) {
+function searchWarning(warningMessage) { // Function to create warnings for form submissions
     const formSubmitBtn = document.querySelector("button");
-    formSubmitBtn.disabled = true;
+    formSubmitBtn.disabled = true; // Disable submit button when a warning appears
 
     const warningContainer = document.querySelector(".warning-container"); // Find warning container
     const warningElement = document.querySelector(".warning-message"); // Find warning message p element
     warningElement.textContent = warningMessage;
-    warningContainer.classList.add("active"); // Make container visable
+    warningContainer.classList.add("active"); // Make warning visable
 
     setTimeout(() => {
-        warningContainer.classList.remove("active"); // Make container invisable after 10s
+        warningContainer.classList.remove("active"); // Make warning invisable after 10s
         setTimeout(() => {
             warningElement.textContent = ""; // Remove text content once transition is done
-            formSubmitBtn.disabled = false;
+            formSubmitBtn.disabled = false; // Enable submit button when a warning disappears
         }, 500);
     }, 2000);
 }
 
-async function requestCoinInfo (coinSeach, currency)  {
-    if (coinSeach.length <= 5) {
+async function requestCoinInfo (coinSeach, currency) { // Functionn to retrieve coin infomation from api
+    if (coinSeach.length <= 5) { // Make sure user is searching for coin name instead for symbol E.g. "bitcoin" instead of "BTC"
         searchWarning("Please enter full coin name !");
     } else {
         try {
-    
             const res = await axios.get(`https://api.coingecko.com/api/v3/coins/markets`, {
                 params: {
                     ids: coinSeach,
@@ -148,7 +145,7 @@ async function requestCoinInfo (coinSeach, currency)  {
                 }
             });
     
-            if (!(res.data.length === 0)) {
+            if (!(res.data.length === 0)) { // If API returns a search result build coin object using data retrieved
                 for (const coin of res.data) {
                     const coinObject = {
                         "id": coin.coinSeach,
@@ -160,12 +157,12 @@ async function requestCoinInfo (coinSeach, currency)  {
                         "image": coin.image,
                         "last-updated": coin.last_updated
                     };
-                    buildTile(coinObject, currency);
+                    buildTile(coinObject, currency); // Build tile in DOM using coin object
                 }
-            } else {
+            } else { // Creating warning message if no data is found for coin/currency pairing
                 searchWarning(`No data for ${coinSeach.toUpperCase()}/${currency.toUpperCase()} !`);
             }
-        } catch (e) {
+        } catch (e) { // Creating warning message if no data is found for currency searched
             searchWarning(`No data for ${currency.toUpperCase()} markets !`);
             console.log("ERROR", e);
         }
@@ -174,17 +171,17 @@ async function requestCoinInfo (coinSeach, currency)  {
 
 
 const searchForm = document.querySelector("#searchForm");
-searchForm.addEventListener('submit', function (e) {
-    e.preventDefault();
-    const coinInput = searchForm.elements.coin;
+searchForm.addEventListener('submit', function (e) { // Define what happens when form is submitted
+    e.preventDefault(); // Disable default submisson event behaviour
+    const coinInput = searchForm.elements.coin; // Get user coin search
     const coinSeach = coinInput.value.toLowerCase();
 
-    const currencyInput = searchForm.elements.currency;
+    const currencyInput = searchForm.elements.currency; // Get user currency search
     const currencySearch = currencyInput.value;
     
-    if (coinSeach && currencySearch) {
+    if (coinSeach && currencySearch) { // If user has searched for coin & currency request data for pairing from API
         requestCoinInfo(coinSeach, currencySearch);
-    } else {
+    } else { // Create Custom warning messages based on incorrect searches
         let warningMessage = "";
         if (!coinSeach && !currencySearch) {
             warningMessage = "Coin and currency required !";
@@ -196,6 +193,6 @@ searchForm.addEventListener('submit', function (e) {
         searchWarning(warningMessage);
     }
 
-    coinInput.value = "";
+    coinInput.value = ""; // Reset input fields
     currencyInput.value = "";
 });
